62eff08c418d09330c4b93d91a63362017713e9b
20120386 <tuanlam689@gmail.com>
2023-06-17 14:08:02 +0700

    useEffect() with fake comment

--- TikTok-Clone/tiktok/src/Content.js
+++ TikTok-Clone/tiktok/src/Content.js
@@ -1,19 +1,53 @@
 import { useState, useEffect } from "react";
 
+const courses = [
+    {
+        id: 1,
+        name: 'HTML, CSS cơ bản'
+    },
+    {
+        id: 2,
+        name: 'Javascript cơ bản'
+    },
+    {
+        id: 3,
+        name: 'ReactJS cơ bản'
+    }
+]
+
 function Content() {
-    const [countdown, setCountdown] = useState(180);
+    
+    const [selectedId, setSelectedId] = useState(1);
 
     useEffect(() => {
-        const timerID = setInterval(() => {
-            setCountdown(prevState => prevState - 1);
-        }, 1000);
 
-        return () => clearInterval(timerID);
-    }, []);
+        const handleComment = ({ detail }) => {
+            console.log(detail);
+        }
+
+        window.addEventListener(`lesson-${selectedId}`, handleComment);
+
+        return () => {
+            window.removeEventListener(`lesson-${selectedId}`, handleComment);
+        }
+
+    }, [selectedId])
 
     return (
         <div>
-            <h1>{countdown}</h1>
+            <ul>
+                {courses.map(course => 
+                    (
+                        <li 
+                            key={course.id}
+                            style={
+                                {color: selectedId === course.id ? 'red' : '#333'}
+                            }
+                            onClick={() => setSelectedId(course.id)}
+                        >{course.name}</li>
+                    )
+                )}
+            </ul>
         </div>
     )
 }
