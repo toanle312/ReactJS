bbe60c9eb3ca713cec4e4b34be82b8f871d3ebc5
20120386 <tuanlam689@gmail.com>
2023-06-19 11:32:56 +0700

    useReducer() Hook with TodoList

--- TikTok-Clone/tiktok/src/App.js
+++ TikTok-Clone/tiktok/src/App.js
@@ -1,29 +1,98 @@
 // import './App.css';
-import { useState, useReducer } from "react";
+import { useRef, useReducer } from "react";
 
-const initState = 0;
+const initState = {
+  todo: '',
+  todoList: ['HTML, CSS', 'JavaScript', 'ReactJS']
+};
 
-const UP_ACTION = "up";
-const DOWN_ACTION = "down";
+const SET_JOB = 'set_job';
+const ADD_JOB = 'add_job';
+const DELETE_JOB = 'delete_job';
+
+const setJob = payload => {
+  return {
+    type: SET_JOB,
+    payload
+  }
+}
+
+const addJob = payload => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+
+const deleteJob = payload => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
 
 const reducer = (state, action) => {
-  switch (action) {
-    case UP_ACTION:
-      return state + 1;
-    case DOWN_ACTION:
-      return state - 1;
+  switch (action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        todo: action.payload
+      };
+    case ADD_JOB:
+      return {
+        ...state,
+        todoList: action.payload ? [...state.todoList, action.payload] : [...state.todoList]
+      };
+    case DELETE_JOB:
+      return {
+        ... state,
+        todoList: [...state.todoList].filter((todo, index) => index !== action.payload)
+      }
     default:
       throw new Error(`Invalid action`);
   }
-};
+  
+}
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initState);
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const {todo, todoList} = state;
+
+  const inputRef = useRef();
+
+  const handelSubmit = () => {
+    dispatch(addJob(todo));
+    dispatch(setJob(''));
+    inputRef.current.focus();
+  }
+
   return (
     <div className="App" style={{ padding: "20px" }}>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
-      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+      <input 
+        ref={inputRef}
+        placeholder="Enter todo ..."
+        value={todo}
+        onChange={e => {
+          dispatch(setJob(e.target.value.trim()));
+        }}
+      />
+      <button 
+        onClick={handelSubmit}
+      >Add</button>
+      <ul>
+        {todoList.map((todoItem, index) => (
+          <li key={index}>{todoItem}
+            <button
+              style={{ marginLeft: "10px", cursor: "pointer"}}
+              onClick={() => {
+                dispatch(deleteJob(index));
+              }}
+            >&times;</button>
+          </li>
+        )
+        )}
+      </ul>
     </div>
   );
 }
