f045784706f0f9a7d5165f74c857d66702dbe72c
20120386 <tuanlam689@gmail.com>
2023-06-22 18:00:00 +0700

    Navbar modern app

---
+++ modern-app/src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import {Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero} from './Components'
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+
+      <div className="sm:px-16 px-6 flex justify-center items-center">
+        <div className="xl:max-w-[1280px] w-full">
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className="bg-primary flex justify-center items-start">
+        <div className="xl:max-w-[1280px] w-full">
+          <Hero/>
+        </div>
+      </div>
+
+      <div className="bg-primary px-6 flex justify-center items-start">
+        <div className="xl:max-w-[1280px] w-full">
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ modern-app/src/Components/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import { logo, menu, close } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="flex py-6 items-center navbar justify-between">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <ul className="list-none hidden ss:flex justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal 
+            text-[16px] cursor-pointer text-white 
+            ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div
+        className="ss:hidden flex flex-1 justify-end 
+         items-center"
+      >
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px]"
+          onClick={() => {
+            setToggle((prevState) => !prevState);
+          }}
+        />
+        <div
+          className={`${toggle ? "flex" : "hidden"} 
+          bg-black-gradient p-6 absolute right-0 top-20 
+          mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col
+          justify-center items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal 
+                text-[16px] cursor-pointer text-white 
+            ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
